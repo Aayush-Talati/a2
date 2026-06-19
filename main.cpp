c957b1047ab3f5d95304221b55cc03e484ea027c
Sparsh Mody <97982596+SparshMody@users.noreply.github.com>
2026-06-18 20:53:37 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
-/*
- * Name: Aayush Talati
- * Class account username: atalati6901
- * Class information: CS 570
- * Assignment information: Assignment #2, Experimental Shell (XSH)
- * Filename: main.cpp
- *
- * Program entry point.  The main function is intentionally small so it acts
- * as a table of contents for the shell.
- */
+// CS480 Summer 2026 Term
+// Assignment 2 - main.cpp
+
+// Team Members:
+// 1. Aayush Talati, class account: cssc3161
+// 2. Sparsh Mody, class account: cssc3001
+
+// Account used for testing:
+// cssc3161 / atalati6901
+//Program entry point.  The main function is intentionally small so it acts
+// as a table of contents for the shell.
+ 
 
 #include "xsh.h"
 
